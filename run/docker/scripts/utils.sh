c900720f3fa5673b1a6b00a6f1f1bb1c3835d2f2
umair <umair.fayaz@us.nationalgrid.com>
2025-10-25 17:22:55 +0530

    Update environment configuration for secure server scheme and host IP address

--- run/docker/scripts/utils.sh
+++ run/docker/scripts/utils.sh
@@ -117,7 +117,7 @@ function setDockerComposeCLIOptions () {
 }
 
 function exportHostIP() {
-    export HOST_IP_ADDRESS=https://ozone.indiclinic.in
+    export HOST_IP_ADDRESS=ozone.indiclinic.in
     echo "$INFO IP address set to: $HOST_IP_ADDRESS"
 }
 
